21b8e1adee3490b12c5bc1f2cb2235a059a0722e
itstimi-XD <ssk0710402@gmail.com>
2024-09-25 14:01:06 +0900

    Feat: pointService 기본 기능 작성

---
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -0,0 +1,42 @@
+package io.hhplus.tdd.point
+
+import io.hhplus.tdd.database.PointHistoryTable
+import io.hhplus.tdd.database.UserPointTable
+import org.springframework.stereotype.Service
+
+@Service
+class PointService(
+    private val userPointTable: UserPointTable,
+    private val pointHistoryTable: PointHistoryTable
+) {
+
+    fun getUserPoint(id: Long): UserPoint {
+        return userPointTable.selectById(id)
+    }
+
+    fun chargeUserPoint(id: Long, amount: Long): UserPoint {
+        val userPoint = userPointTable.selectById(id)
+        val newPoint = userPoint.point + amount
+        val updatedUserPoint = userPointTable.insertOrUpdate(id, newPoint)
+        // 이력 추가
+        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis())
+
+        return updatedUserPoint
+    }
+
+    fun useUserPoint(id: Long, amount: Long): UserPoint {
+        val currentPoint = userPointTable.selectById(id)
+        if (currentPoint.point < amount) {
+            throw IllegalArgumentException("잔액이 부족합니다.")
+        }
+        val updatedUserPoint = userPointTable.insertOrUpdate(id, currentPoint.point - amount)
+        // 이력 추가
+        pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis())
+
+        return updatedUserPoint
+    }
+
+    fun getUserPointHistory(userId: Long): List<PointHistory> {
+        return pointHistoryTable.selectAllByUserId(userId)
+    }
+}
